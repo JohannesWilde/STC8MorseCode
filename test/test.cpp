fcd433f4d5cea56c54fed062ad44f03580b95363
Johannes Wilde <git@jowil.de>
2026-04-05 21:01:12 +0200

    Extend test case.

--- test/test.cpp
+++ test/test.cpp
@@ -3,27 +3,68 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
+
+#include <iostream>
+#include <vector>
 
 
 // hacky way to remove storage specifier from STC source code.
 #define __code
 
 const MorseCodeSymbolIndex morseCodeText[] = {
-    morseCodeSymbolIndex_5, /*morseCodeSymbolIndex_1, morseCodeSymbolIndex_2, morseCodeSymbolIndex_3, */morseCodeSymbolIndex_space,morseCodeSymbolIndex_space,
+    morseCodeSymbolIndex_5,
+    morseCodeSymbolIndex_1,
+    morseCodeSymbolIndex_space,
+    morseCodeSymbolIndex_space,
+    morseCodeSymbolIndex_2,
+    morseCodeSymbolIndex_3,
 };
 MorseCodeSenderState morseCodeSenderState;
 
 #include "morsecode.c"
 
 
+static void printVector(std::vector<bool> const & values)
+{
+    for (bool const value : values)
+    {
+        if (value)
+        {
+            std::cout << "^";
+        }
+        else
+        {
+            std::cout << "_";
+        }
+    }
+}
+
+
+#define DIT true
+#define DAH true, true, true
+#define PAUSE_1 false
+#define PAUSE_3 false, false, false
+#define PAUSE_7 false, false, false, false, false, false, false
 
 int main()
 {
+    std::vector<bool> const expectations{
+        /*morseCodeSymbolIndex_5*/ DIT, PAUSE_1, DIT, PAUSE_1, DIT, PAUSE_1, DIT, PAUSE_1, DIT,
+                                   PAUSE_3,
+        /*morseCodeSymbolIndex_1*/ DIT, PAUSE_1, DAH, PAUSE_1, DAH, PAUSE_1, DAH, PAUSE_1, DAH,
+        /*morseCodeSymbolIndex_space*/ PAUSE_7,
+        /*morseCodeSymbolIndex_space*/ PAUSE_7,
+        /*morseCodeSymbolIndex_2*/ DIT, PAUSE_1, DIT, PAUSE_1, DAH, PAUSE_1, DAH, PAUSE_1, DAH,
+        /*morseCodeSymbolIndex_3*/ DIT, PAUSE_1, DIT, PAUSE_1, DIT, PAUSE_1, DAH, PAUSE_1, DAH,
+    };
+    std::vector<bool> observations;
+    observations.reserve(expectations.size());
+
+
+
     morseCodeSenderStateInit();
 
-
-    for (size_t index = 0; 50 > index; ++index)
+    for (size_t index = 0; expectations.size() > index; ++index)
     {
         if (morseCodeSenderStateUpdate())
         {
@@ -34,17 +75,51 @@ int main()
             // intentionally empty
         }
 
-        if (morseCodeSenderState.showingSignalAndNotPause)
-        {
-            printf("^");
-        }
-        else
-        {
-            printf("_");
-        }
+        observations.push_back(morseCodeSenderState.showingSignalAndNotPause);
     }
 
-    printf("\n");
 
-    return 0;
+    bool success = false;
+
+    if (expectations.size() != observations.size())
+    {
+        // success = false;
+    }
+    else
+    {
+        bool equal = true;
+        for (size_t index = 0; expectations.size() > index; ++index)
+        {
+            if (expectations[index] != observations[index])
+            {
+                equal = false;
+                break;
+            }
+            else
+            {
+                // intentionally empty
+            }
+        }
+        success = equal;
+    }
+
+
+    if (not success)
+    {
+        std::cout << "observations: ";
+        printVector(observations);
+        std::cout << std::endl;
+
+        std::cout << "expectations: ";
+        printVector(expectations);
+        std::cout << std::endl;
+
+        std::cout << "Failed." << std::endl;
+    }
+    else
+    {
+        std::cout << "Success." << std::endl;
+    }
+
+    return success ? 0 : -1;
 }
